1a1a5cf5a39603cb608081ae71b091fdf499f39e
George O <geoorge191@hotmail.com>
2019-08-08 11:36:21 -0500

    Primer avance

---
+++ SES_Existencias/Formularios/Frm_Existencias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SES_Existencias.Formularios
+{
+    public partial class Frm_Existencias : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Existencias()
+        {
+            InitializeComponent();
+        }
+    }
+}
